8b682b4cdf6d14600c87658afc3ee86630f692b4
fralcy <quangthinh2924@gmail.com>
2024-04-18 23:43:08 +0700

    update loginviewmodel

--- mvvm demo/ViewModels/LoginViewModel.cs
+++ mvvm demo/ViewModels/LoginViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Security;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -9,6 +12,41 @@ namespace mvvm_demo.ViewModels
 {
     public class LoginViewModel : ViewModelBase
     {
+        //Fields
+        private string _username;
+        private SecureString _password;
 
+        //Properties
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+            }
+        }
+        public SecureString Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+        //Command
+        public ICommand LoginCommand { get; }
+        //Constructor
+        public LoginViewModel()
+        {
+           
+        }
     }
 }
